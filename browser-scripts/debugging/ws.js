34a8b6ccfb33b025e6b175562bed6909d1e92193
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2024-03-23 20:40:39 +1100

    Create ws.js

---
+++ browser-scripts/debugging/ws.js
@@ -0,0 +1,17 @@
+(async() => {
+    'use strict';
+
+    // Your code here...
+    setTimeout(function() {
+        window.jd.ws.send = new Proxy(window.jd.ws.send, {
+            apply: function(target, scope, args) {
+                if(typeof(args[0]) === 'string') {
+                    console.log(args[0])
+                }
+                let data = target.apply(scope, args);
+                return data;
+            }
+        });
+        window.jd.ws.addEventListener('message', async(e) => { console.log(e.data); });
+    },6000);
+})(); // setup the JDNService watcher
